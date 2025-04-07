ddbd7d0daf800d8dbeb18619814df949e3a5a75e
Swapnil Uppin <114418519+Deadpoolpool@users.noreply.github.com>
2025-04-07 22:50:19 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker push snoop82/sample-python-codebuild-service
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 snoop82/sample-python-codebuild-service
 
